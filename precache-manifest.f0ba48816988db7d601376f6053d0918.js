65c920faf721c56a94e18e96891e7ef07e8306c7
Emilio R <reyesdasix181@gmail.com>
2019-05-15 22:08:51 +0000

    Updates

---
+++ precache-manifest.f0ba48816988db7d601376f6053d0918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619dea2c167f46d12d604d9a09849eb1",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "77fbe53bd98946181fec",
+    "url": "/ContactManager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6f0f10c78f51d4a4c1ad",
+    "url": "/ContactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "77fbe53bd98946181fec",
+    "url": "/ContactManager/static/js/2.e40046aa.chunk.js"
+  },
+  {
+    "revision": "6f0f10c78f51d4a4c1ad",
+    "url": "/ContactManager/static/js/main.7ad33c8e.chunk.js"
+  },
+  {
+    "revision": "112eb2a9bc0338f97a28",
+    "url": "/ContactManager/static/js/runtime~main.0e09fb07.js"
+  }
+]);
